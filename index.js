10b60e3a8bfe73a4e3264bc52191805f67a0d122
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-11-15 07:07:25 +0200

    feat(deps): use named-level-store

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const swarm = require('hyperdrive-archive-swarm')
+const level = require('named-level-store')
 const hypercore = require('hypercore')
 const assert = require('assert')
-const mkdirp = require('mkdirp')
-const level = require('level')
 const path = require('path')
 const fs = require('fs')
 
@@ -18,11 +17,10 @@ function Normcore (name) {
     ? name
     : new Buffer(name).toString('hex')
 
-  const location = path.join(process.env.HOME, '.normcore', name)
-  mkdirp.sync(location)
+  const db = level(name)
 
-  const secretKeyPath = path.join(location, 'SECRET_KEY')
-  const keyPath = path.join(location, 'KEY')
+  const secretKeyPath = path.join(db.location, 'SECRET_KEY')
+  const keyPath = path.join(db.location, 'KEY')
   var defaultSecretKey = null
   var defaultKey = null
 
@@ -32,7 +30,7 @@ function Normcore (name) {
   }
 
   const feedKey = isHex ? name : defaultKey
-  const feed = hypercore(level(location)).createFeed(feedKey, {
+  const feed = hypercore(db).createFeed(feedKey, {
     secretKey: defaultSecretKey
   })
 
