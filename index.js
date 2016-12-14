57317d22bc5a9ee28cd037f0aabeea624928b972
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-12-14 21:13:54 +0100

    feat(index): more asserts

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const swarm = require('hyperdiscovery')
 const level = require('named-level-store')
 const hypercore = require('hypercore')
 const assert = require('assert')
+const xtend = require('xtend')
 const path = require('path')
 const fs = require('fs')
 
 module.exports = Normcore
 
 function Normcore (name, opts) {
+  opts = opts || {}
+
   assert.equal(typeof name, 'string', 'normcore: name must be a string')
+  assert.equal(typeof opts, 'object', 'normcore: opts must be an object')
+
   const isHex = /^[0-9a-f]{64}$/.test(name)
   name = (isHex)
     ? name
@@ -26,10 +31,9 @@ function Normcore (name, opts) {
     defaultSecretKey = fs.readFileSync(secretKeyPath)
   }
 
+  const _opts = xtend(opts, { secretKey: defaultSecretKey })
   const feedKey = isHex ? name : defaultKey
-  const feed = hypercore(db).createFeed(feedKey, {
-    secretKey: defaultSecretKey
-  })
+  const feed = hypercore(db).createFeed(feedKey, _opts)
 
   if (!isHex) {
     fs.writeFileSync(secretKeyPath, feed.secretKey)
